0b4c96ced836b73cc2bbbb24c986943b87e8633c
Jeremiah Lachica <jeremiahlachica@outlook.com>
2024-03-13 19:43:50 +0800

    feat(app-core): contants | add EnvironmentType

---
+++ packages/app-core/src/constant/EnvironmentType.ts
@@ -0,0 +1,6 @@
+export enum EnvironmentType {
+  Develop = 'dev',
+  Test = 'test',
+  Staging = 'staging',
+  Production = 'production',
+}
--- packages/app-core/src/constant/index.ts
+++ packages/app-core/src/constant/index.ts
@@ -1,3 +1,4 @@
 export * from './AuthProviderId';
 export * from './DateUnitId';
+export * from './EnvironmentType';
 export * from './Name';
