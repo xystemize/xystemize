d673413b74a7cd68ff3d004450071807b372aab9
Jeremiah Lachica <jeremiahlachica@outlook.com>
2024-03-15 20:55:30 +0800

    feat(xytemize): generator | backend-app | add process to update index.ts

--- packages/xystemize/src/generators/backend-api/generator.ts
+++ packages/xystemize/src/generators/backend-api/generator.ts
@@ -3,6 +3,8 @@ import { toClassName, toPropertyName } from '@xystemize/app-core';
 import { kebabCase, trim } from 'lodash';
 import * as path from 'path';
 
+import { appendNxGeneratedFile } from '../../utility';
+
 import { BackendApiGeneratorSchema } from './schema';
 
 export async function backendApiGenerator(tree: Tree, options: BackendApiGeneratorSchema) {
@@ -22,6 +24,22 @@ export async function backendApiGenerator(tree: Tree, options: BackendApiGenerat
   const projectRoot = `${resolvedOptions.directory}/${resolvedOptions.folderName}`;
 
   generateFiles(tree, path.join(__dirname, 'files'), projectRoot, resolvedOptions);
+
+  const indexFilePath = `${defaultDirectory}/index.ts`;
+
+  appendNxGeneratedFile({
+    tree,
+    filePath: indexFilePath,
+    pattern: '// ### Imports:End ###',
+    fileContent: `import { ${resolvedOptions.name} } from './${folderName}/${resolvedOptions.name}';`,
+  });
+
+  appendNxGeneratedFile({
+    tree,
+    filePath: indexFilePath,
+    pattern: '// ### APIs:End ###',
+    fileContent: `export const ${resolvedOptions.nameLowerCase} = regularFunction.onRequest(${resolvedOptions.name});`,
+  });
 
   await formatFiles(tree);
 }
