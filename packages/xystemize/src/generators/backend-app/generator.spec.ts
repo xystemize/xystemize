f3c92365b015af3cd663968c3bfe81a85e676487
Jeremiah Lachica <jeremiahlachica@outlook.com>
2024-03-16 19:53:54 +0800

    test(xystemize): generator | backend-app | add file assertions

--- packages/xystemize/src/generators/backend-app/generator.spec.ts
+++ packages/xystemize/src/generators/backend-app/generator.spec.ts
@@ -1,7 +1,8 @@
 import { readProjectConfiguration, Tree } from '@nx/devkit';
 import { createTreeWithEmptyWorkspace } from '@nx/devkit/testing';
+import { isCharExistsInString } from '@xystemize/app-core';
 
-import { readNxGeneratedFile, readNxGeneratedJsonFile } from '../../utility';
+import { fileShouldNotExists, readNxGeneratedFile, readNxGeneratedJsonFile } from '../../utility';
 
 import { backendAppGenerator } from './generator';
 import { BackendAppGeneratorSchema } from './schema';
@@ -19,12 +20,31 @@ describe('backend-app generator', () => {
     const config = readProjectConfiguration(tree, 'test');
     expect(config).toBeDefined();
 
-    const projectJson = readNxGeneratedJsonFile({ tree, filePath: `apps/${options.name}/project.json` });
+    const projectRoot = `apps/${options.name}`;
+
+    const projectJson = readNxGeneratedJsonFile({ tree, filePath: `${projectRoot}/project.json` });
     expect(projectJson.targets.build.options.generatePackageJson).toBe(true);
+    expect(projectJson.targets.build.options.main).toBe(`${projectRoot}/src/index.ts`);
+    expect(projectJson.targets.build.options.assets).toStrictEqual([`${projectRoot}/src/@assets`]);
+
     expect(projectJson.targets.killports).toBeDefined();
     expect(projectJson.targets.deploy).toBeDefined();
 
-    const index = readNxGeneratedFile({ tree, filePath: `apps/${options.name}/src/index.ts` });
+    const index = readNxGeneratedFile({ tree, filePath: `${projectRoot}/src/index.ts` });
     expect(index).toBeDefined();
+    expect(isCharExistsInString({ char: `import { Apiv1 } from './@api-v1/Apiv1';`, string: index })).toBeTruthy();
+    expect(
+      isCharExistsInString({ char: 'export const apiv1 = regularFunction.onRequest(Apiv1);', string: index })
+    ).toBeTruthy();
+
+    const apiv1 = readNxGeneratedFile({ tree, filePath: `${projectRoot}/src/@api-v1/ApiV1.ts` });
+    expect(apiv1).toBeDefined();
+
+    const gitkeepInRenamedAssetsFolder = readNxGeneratedFile({ tree, filePath: `${projectRoot}/src/@assets/.gitkeep` });
+    expect(gitkeepInRenamedAssetsFolder).toBe('');
+
+    fileShouldNotExists({ tree, filePath: `${projectRoot}/src/assets/.gitkeep` });
+    fileShouldNotExists({ tree, filePath: `${projectRoot}/src/main.ts` });
+    fileShouldNotExists({ tree, filePath: `${projectRoot}/src/app/app.module.ts` });
   });
 });
