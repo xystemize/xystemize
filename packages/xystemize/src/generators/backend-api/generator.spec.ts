92faf9d46347aa3f0398e67a579ac61b55ce8080
Jeremiah Lachica <jeremiahlachica@outlook.com>
2024-03-17 14:02:21 +0800

    test(xystemize): generator | test backend-api

--- packages/xystemize/src/generators/backend-api/generator.spec.ts
+++ packages/xystemize/src/generators/backend-api/generator.spec.ts
@@ -1,5 +1,8 @@
 import { Tree } from '@nx/devkit';
 import { createTreeWithEmptyWorkspace } from '@nx/devkit/testing';
+import { isCharExistsInString } from '@xystemize/app-core';
+
+import { fileShouldExists, readNxGeneratedFile } from '../../utility';
 
 import { backendApiGenerator } from './generator';
 import { BackendApiGeneratorSchema } from './schema';
@@ -13,6 +16,18 @@ describe('backend-api generator', () => {
   });
 
   it('should run successfully', async () => {
+    const projectRoot = `apps/backend`;
+    const componentRoot = `${projectRoot}/src/${options.name}`;
+
     await backendApiGenerator(tree, options);
+
+    const index = readNxGeneratedFile({ tree, filePath: `${projectRoot}/src/index.ts` });
+    expect(index).toBeDefined();
+    expect(isCharExistsInString({ char: `import { Api } from './api/Api';`, string: index })).toBeTruthy();
+    expect(
+      isCharExistsInString({ char: 'export const api = regularFunction.onRequest(Api);', string: index })
+    ).toBeTruthy();
+
+    fileShouldExists({ tree, filePath: `${componentRoot}/Api.ts` });
   });
 });
