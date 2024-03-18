c6f3f328d10972d603677810877107dbcabb58c7
Jeremiah Lachica <jeremiahlachica@outlook.com>
2024-03-18 11:51:51 +0800

    fix(xystemize): generator | js | fix directory

--- packages/xystemize/src/generators/js-lib/generator.ts
+++ packages/xystemize/src/generators/js-lib/generator.ts
@@ -9,23 +9,20 @@ import { appendNxGeneratedJsonFile } from '../../utility/GeneratorUtility';
 import { JsLibGeneratorSchema } from './schema';
 
 export async function jsLibGenerator(tree: Tree, options: JsLibGeneratorSchema) {
-  const { name, directory } = options;
+  const { name, directory = 'libs' } = options;
   const packageJson = readJson(tree, 'package.json');
   const scopeName = replace(packageJson.projectName ?? packageJson.name ?? '', '/source', '');
   const libName = names(name).fileName;
   const resolvedOptions = {
     ...options,
     name: libName,
+    directory: directory,
     pascalCaseFiles: true,
     publishable: true,
     importPath: `${scopeName}/${libName}`,
   };
 
-  let projectRoot = `libs/${libName}`;
-
-  if (directory) {
-    projectRoot = `libs/${directory}/${libName}`;
-  }
+  const projectRoot = `${resolvedOptions.directory}/${libName}`;
 
   await libraryGenerator(tree, resolvedOptions as LibraryGeneratorSchema);
   generateFiles(tree, path.join(__dirname, 'files'), projectRoot, resolvedOptions);
