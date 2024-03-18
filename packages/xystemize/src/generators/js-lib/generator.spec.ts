7ed1d15b0e2bfb43328622cc8ab81acd70563c17
Jeremiah Lachica <jeremiahlachica@outlook.com>
2024-03-18 13:19:57 +0800

    test(xystemize): generator | update test for jsLibGenerator()

--- packages/xystemize/src/generators/js-lib/generator.spec.ts
+++ packages/xystemize/src/generators/js-lib/generator.spec.ts
@@ -8,23 +8,28 @@ import { JsLibGeneratorSchema } from './schema';
 
 describe('js-lib generator', () => {
   let tree: Tree;
-  const options: JsLibGeneratorSchema = { name: 'TestTest' };
+  const options: JsLibGeneratorSchema = {
+    name: 'TestTest',
+    compiler: 'tsc',
+  };
 
   beforeEach(() => {
     tree = createTreeWithEmptyWorkspace();
   });
 
   test('jsLibGenerator()', async () => {
+    const libName = names(options.name).fileName;
+    const libRoot = `libs/${libName}`;
+
     await jsLibGenerator(tree, options);
 
-    const libName = names(options.name).fileName;
     const config = readProjectConfiguration(tree, libName);
     expect(config).toBeDefined();
 
-    const packageJson = readNxGeneratedJsonFile({ tree, filePath: `${libName}/package.json` });
+    const packageJson = readNxGeneratedJsonFile({ tree, filePath: `${libRoot}/package.json` });
     expect(packageJson.publishConfig).toStrictEqual({ access: 'public' });
 
-    const projectJson = readNxGeneratedJsonFile({ tree, filePath: `${libName}/project.json` });
+    const projectJson = readNxGeneratedJsonFile({ tree, filePath: `${libRoot}/project.json` });
     expect(projectJson.release).toStrictEqual({
       executor: 'nx-release:build-update-publish',
       options: {
