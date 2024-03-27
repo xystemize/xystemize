f53b8bf31c619ecce7d5578b5285191ab7aa7b5d
Jeremiah Lachica <jeremiahlachica@outlook.com>
2024-03-27 13:04:14 +0800

    refactor(backend): update firestore transaction

--- packages/backend/src/module/firestore/FirestoreTransaction.ts
+++ packages/backend/src/module/firestore/FirestoreTransaction.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { AppBaseDataModel } from '@xystemize/app-core';
+import { AppBaseDataModel, Name } from '@xystemize/app-core';
 import { firestore } from 'firebase-admin';
 
 import { FirebaseService } from '../firebase';
@@ -7,29 +7,58 @@ import { FirebaseService } from '../firebase';
 export type AppBaseDocument = AppBaseDataModel & { id: string };
 
 export class FirestoreTransaction {
-  transactions: { type: string; documentRef: firestore.DocumentReference; data: Record<string, any> }[] = [];
+  transactions: { type: string; documentRef: firestore.DocumentReference; data?: Record<string, any> }[] = [];
 
-  private addTransaction = (data: AppBaseDocument, type: string) => {
+  private addTransaction = ({
+    collectionName,
+    docId,
+    data,
+    type,
+  }: {
+    collectionName: string;
+    docId: string;
+    data?: Record<string, any>;
+    type: string;
+  }) => {
     this.transactions.push({
       type: type,
-      documentRef: FirebaseService.firestore.collection(data.firestoreCollectionName).doc(data.id),
-      data: data.toFirestoreDocument(),
+      data: data,
+      documentRef: FirebaseService.firestore.collection(collectionName).doc(docId),
     });
   };
 
   create = (data: AppBaseDocument) => {
-    this.addTransaction(data, 'create');
+    this.addTransaction({
+      collectionName: data[Name.collectionName],
+      docId: data.id,
+      data: data.toCreateObject(),
+      type: 'create',
+    });
   };
 
   set = (data: AppBaseDocument) => {
-    this.addTransaction(data, 'set');
+    this.addTransaction({
+      collectionName: data[Name.collectionName],
+      docId: data.id,
+      data: data.toCreateObject(),
+      type: 'set',
+    });
   };
 
   update = (data: AppBaseDocument) => {
-    this.addTransaction(data, 'update');
+    this.addTransaction({
+      collectionName: data[Name.collectionName],
+      docId: data.id,
+      data: data.toUpdateObject(),
+      type: 'update',
+    });
   };
 
   delete = (data: AppBaseDocument) => {
-    this.addTransaction(data, 'delete');
+    this.addTransaction({
+      collectionName: data[Name.collectionName],
+      docId: data.id,
+      type: 'delete',
+    });
   };
 }
